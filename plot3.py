c5d471a1c2d7ef069a14aa67e02da5becbd49e72
Lauritz Klünder <lauritz.kluender@tu-dortmund.de>
2017-11-10 18:29:51 +0100

    lalala

---
+++ plot3.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+V1, y, x = np.genfromtxt('data3.txt', unpack=True)
+
+def f(x, a, b):
+    return a * x + b
+
+params, covariance_matrix = curve_fit(f, x, y)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+x_plot = np.linspace(-0.01, 0.2, 6)
+
+plt.plot(x, y, 'kx', label='Daten')
+plt.plot(x_plot, f(x_plot, *params), 'r-', label='Linearer Fit')
+plt.legend()
+plt.grid()
+plt.ylabel('D(x) in m')
+plt.xlabel('*')
+plt.show()
+#plt.savefig('ausgleichsgerade.pdf')
+print('a=', params[0], '+-', errors[0])
+print('b=', params[1], '+-', errors[1])
